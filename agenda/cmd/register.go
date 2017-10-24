83190a55af717d4092a8a4cd1b5b967cd06e64ed
caijh23 <caijh23@gmail.com>
2017-10-24 22:05:25 +0800

    agenda not json

--- agenda/cmd/register.go
+++ agenda/cmd/register.go
@@ -17,29 +17,96 @@ package cmd
 import (
 	"fmt"
 
+	"Go/agenda/entity"
 	"github.com/spf13/cobra"
+	"os"
 )
 
 // registerCmd represents the register command
 var registerCmd = &cobra.Command{
 	Use:   "register",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "To register an account",
+	Long:  `To regist your account,the following parameters should be avaliable,u/user is your username,p/password is your password,e/email is your email,P/Phone is your phone`,
 	Run: func(cmd *cobra.Command, args []string) {
-		username, _ := cmd.Flags().GetString("user")
-		fmt.Println("register called by " + username)
+		username, err1 := cmd.Flags().GetString("user")
+		password, err2 := cmd.Flags().GetString("password")
+		email, err3 := cmd.Flags().GetString("email")
+		phone, err4 := cmd.Flags().GetString("phone")
+		tempErr := []error{err1, err2, err3, err4}
+		isempty := outPutErr(tempErr)
+		if !isempty {
+			fmt.Println("Register Fail!")
+			os.Exit(1)
+		}
+		ifNull(username, password, email, phone)
+		ifExist(username)
+		var tempUser entity.User
+		tempUser.SetUser(username, password, email, phone)
+		s := &entity.Storage
+		s.CreatUser(tempUser)
+
+		fmt.Println("Register Success!")
+		fmt.Println("Your information is\nusername: " + username + "\npassword: " + password + "\nemail: " + email + "\ntelephone: " + phone)
 	},
+}
+
+func getAll(u entity.User) bool { return true }
+
+func switcher(u *entity.User) { u.SetPassword("laji") }
+
+func ifNull(u string, p string, e string, t string) {
+	i := 0
+	err := ""
+	if u == "" {
+		err += "\nYou must input your name."
+		i += 1
+	}
+	if p == "" {
+		err += "\nYou must input your phone."
+		i += 1
+	}
+	if e == "" {
+		err += "\nYou must input your email."
+		i += 1
+	}
+	if t == "" {
+		err += "\nYou must input your phone."
+		i += 1
+	}
+	if i != 0 {
+		fmt.Println("Register Fail!" + err)
+		os.Exit(2)
+	}
+}
+
+func ifExist(u string) {
+	s := &entity.Storage
+	userList := s.QueryUser(getAll)
+	for _, v := range userList {
+		if (&v).GetUsername() == u {
+			fmt.Println("Register Fail!")
+			fmt.Println("Your name " + u + " is already exist.")
+			os.Exit(3)
+		}
+	}
+}
+
+func outPutErr(errs []error) bool {
+	for _, value := range errs {
+		if value != nil {
+			fmt.Println(value)
+			return false
+		}
+	}
+	return true
 }
 
 func init() {
 	RootCmd.AddCommand(registerCmd)
-	registerCmd.Flags().StringP("user", "u", "Anonymous", "Help message for username")
-
+	registerCmd.Flags().StringP("user", "u", "", "Help message for username")
+	registerCmd.Flags().StringP("password", "p", "", "the password,it should not be empty")
+	registerCmd.Flags().StringP("email", "e", "", "the email,,it should be empty")
+	registerCmd.Flags().StringP("phone", "t", "", "your phone,it should not be empyt")
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
